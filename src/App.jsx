f5e4ddc8048585300f196204cfbf2626ce0b0110
Chaten Mundle <chetanmundle7875@gmail.com>
2024-04-08 18:50:29 +0530

    chetan

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import "./App1.css";
+import Navbar from "./components/Navbar";
 
 function App() {
- 
-
   return (
     <>
-     <Navbar  />
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
