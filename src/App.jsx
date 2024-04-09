b9c13e81c797c715e5ba5d0b1bf539e6d531804f
Hrishikesh Deshmukh <59472106+HrishikeshDeshmukh@users.noreply.github.com>
2024-04-09 14:23:06 +0530

    new chance

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import './App1.css'
+import Carousel from './components/Carousel';
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
      <Navbar  />
+     <Carousel />
     </>
   );
 }
